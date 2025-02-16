58845bcf5096ab038c7d70017ea6d491cde6d5b5
turtle-coder-woder <turtlecoderwoder@gmail.com>
2025-02-16 15:00:34 +0530

    spring basic app

---
+++ repo-warden/src/main/java/com/repo/warden/repo_warden/RepoWardenApplication.java
@@ -0,0 +1,13 @@
+package com.repo.warden.repo_warden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoWardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoWardenApplication.class, args);
+	}
+
+}
---
+++ repo-warden/src/test/java/com/repo/warden/repo_warden/RepoWardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo.warden.repo_warden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoWardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
